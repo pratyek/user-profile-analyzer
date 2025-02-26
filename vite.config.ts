b18ef362ef67101d11aee092be7e0b41a20b1830
Your Name <your-email@example.com>
2025-02-27 03:55:01 +0530

    node modules added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Smile_Lab/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
