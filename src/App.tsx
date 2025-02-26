93c688d26f3b8f96918d33a43072cb5d502b3d66
Your Name <your-email@example.com>
2025-02-27 04:01:50 +0530

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { useState, useEffect, useRef } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// import { HashRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 // Page Components
